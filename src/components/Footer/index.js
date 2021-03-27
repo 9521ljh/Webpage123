d8e8633bd24027be4901d83cada9a212c73aff70
9521ljh <9521ljh@gmail.com>
2021-03-27 17:26:57 +0900

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {animateScroll as scroll} from 'react-scroll';
+import {FaFacebook, FaInstagram, FaTwitter, FaLinkedin} from 'react-icons/fa';
+import { animateScroll } from 'react-scroll';
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink } from './FooterElement';
+
+const Footer = () => {
+
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    };
+
+    return (
+
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">ATestimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">ATestimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">ATestimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">ATestimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/' onClick={toggleHome}>
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRights>dolla zzz {new Date().getFullYear()} All rights reserved.</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" trarget="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" trarget="_blank"
+                            aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.twitter.com/elon-musk" trarget="_blank"
+                            aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" trarget="_blank"
+                            aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    );
+}
+
+
+
+
+export default Footer
+
+
+
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,68 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true, 
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited',
+    buttonLabel: 'Get Started',
+    imgStart: true,
+    img: require('../../images/barbecue.svg').default, 
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+
+export const homeObjTwo = {
+    id: 'discorver',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false, 
+    topLine: 'A',
+    headline: 'B',
+    description: 'G',
+    buttonLabel: 'Learn more',
+    imgStart: true,
+    img: require('../../images/camping.svg').default, 
+    alt: 'Piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false, 
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited',
+    buttonLabel: 'Start Now',
+    imgStart: true,
+    img: require('../../images/shopping.svg').default, 
+    alt: 'Paper',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjFour = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false, 
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited',
+    buttonLabel: 'Start Now',
+    imgStart: true,
+    img: require('../../images/shopping.svg').default, 
+    alt: 'Paper',
+    dark: false,
+    primary: false,
+    darkText: true
+}
